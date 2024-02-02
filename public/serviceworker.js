f0559aceff64e9948b0d05610b689ece1f346c82
Ruben Hernandez <hernandzruben9@gmail.com>
2024-02-02 01:08:31 -0500

    z

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,52 +1,54 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
-    '/offline',
-    '/css/app.css',
-    '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    "/offline",
+    "/css/app.css",
+    "/js/app.js",
+    "/landing/assets/video/m.mp4",
+    "/landing/assets/video/720.mp4",
+    "/landing/assets/css/style.css",
+    "landing/assets/css/slick.css",
+    "landing/assets/css/fontawesome-all.min.css",
+    "landing/assets/img/logo/logo.png",
+    "auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css",
+    "auth/css/main.css",
+    "auth/css/util.css",
+    "authjs/main.js",
 ];
 
 // Cache on install
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
     this.skipWaiting();
     event.waitUntil(
-        caches.open(staticCacheName)
-            .then(cache => {
-                return cache.addAll(filesToCache);
-            })
-    )
+        caches.open(staticCacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 // Clear cache on activate
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches.keys().then(cacheNames => {
+        caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames
-                    .filter(cacheName => (cacheName.startsWith("pwa-")))
-                    .filter(cacheName => (cacheName !== staticCacheName))
-                    .map(cacheName => caches.delete(cacheName))
+                    .filter((cacheName) => cacheName.startsWith("pwa-"))
+                    .filter((cacheName) => cacheName !== staticCacheName)
+                    .map((cacheName) => caches.delete(cacheName))
             );
         })
     );
 });
 
 // Serve from Cache
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
+        caches
+            .match(event.request)
+            .then((response) => {
                 return response || fetch(event.request);
             })
             .catch(() => {
-                return caches.match('offline');
+                return caches.match("offline");
             })
-    )
+    );
 });
